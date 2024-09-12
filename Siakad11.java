de2633e533fc07ceeb139caeeb4a6c3bc6c09cb2
haf5th <hafiflearning@gmail.com>
2024-09-12 11:03:08 +0700

    no 6

--- Siakad11.java
+++ Siakad11.java
@@ -3,25 +3,27 @@ public class Siakad11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama, nim;
-        char kelas;
+        String kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUts, nilaiUas, nilaiAkhir;
     
         System.out.println("Masukkan nama: ");
         nama = sc.nextLine();
         System.out.println("Masukkan NIM: ");
         nim = sc.nextLine();
         System.out.println("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.println("Masukkan nomor absen: ");
         absen = sc.nextByte();
         System.out.println("Masukkan nilai kuis: ");
         nilaiKuis = sc.nextDouble();
         System.out.println("Masukkan nilai tugas: ");
         nilaiTugas = sc.nextDouble();
-        System.out.println("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        System.out.println("Masukkan nilai uts: ");
+        nilaiUts = sc.nextDouble();
+        System.out.println("Masukkan nilai uas: ");
+        nilaiUas = sc.nextDouble();
+        nilaiAkhir = (0.2 * nilaiKuis + 0.15 * nilaiTugas + 0.3 * nilaiUts + 0.35 * nilaiUas);
         System.out.println("Nama: " + nama + " NIM: " + nim);
         System.out.println("Kelas: " + kelas + " Absen: " + absen);
         System.out.println("Nilai Akhir: " + nilaiAkhir);
